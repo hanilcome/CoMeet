28e74aeb3e8daa45610163eaea71fc73a8f52a4d
0v0baek <qorqor4592@gmail.com>
2023-04-13 11:28:58 +0900

    edit write_view, home
    글쓰기 화면과 홈 함수를 수정했습니다.
    글쓰기 화면의 기본 html 작업을 했습니다.
    
    edit commit/views.py : def write_view
    edit templates/commit/write.html

--- commit/views.py
+++ commit/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 from .models import Commit
 # Create your views here.
 
-
+# 홈 들어갔을 때 뉴스피드로 main.html 띄워주기.
 def home(request):
-    """
-    기본메인페이지. 남는 사람이 하는걸로 !
-    """
+    if request.method == 'GET':
+        all_commit = Commit.objects.all().order_by('-created_at')
+        return render(request, 'commit/main.html', {'commit':all_commit})
+
 
 
 def detail(request):
@@ -17,9 +18,19 @@ def detail(request):
 
 
 def write_view(request):
-    """
-    게시글 쓰기. 백지현
-    """
+    # 화면 띄워주기
+    if request.method == 'GET':
+        return render(request, 'commit/write_view.html')
+    
+    # 데이터베이스에 값 저장
+    if request.method == 'POST':
+        user = request.user
+        my_commit = Commit()
+        my_commit.writer = user
+        my_commit.title = request.POST.get('my-title','')
+        my_commit.content = request.POST.get('my-content','')
+        my_commit.save()
+        return redirect('/')
 
 # 게시글 수정함수
 
