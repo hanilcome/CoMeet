6b3784f624dddf07074d3d8b9744daedd0cfd335
VinylStage <mcpe9869@gmail.com>
2023-04-13 17:02:10 +0900

    test.js생성 templates 수정
    아우 귀차나...
    Co-authored-by: hanilcome <hanilcome@users.noreply.github.com>
    Co-authored-by: Jihyeon Baek <0v0baek@users.noreply.github.com>
    Co-authored-by: ellisajung <ellisajung@users.noreply.github.com>
    Co-authored-by: kim tea yeon <kimty9627@users.noreply.github.com>

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
+
+app_name = 'user'
+
 urlpatterns = [
     path('log_in/', views.log_in_view, name='log_in'),
     path('log_out/', views.log_out_view, name='log_out'),
     path('sign_up/', views.sign_up_view, name='sign_up'),
-    path('my_page/', views.my_page_view, name='my_page'),
+    path('my_page/<int:id>/', views.my_page_view, name='my_page'),
 ]
 """
 디테일 페이지
