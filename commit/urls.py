2c3cca9dd727645a8f0537696f4ccee068fb2b2b
hanilcome <hanilcome@naver.com>
2023-04-16 02:03:23 +0900

    create detail

--- commit/urls.py
+++ commit/urls.py
@@ -4,13 +4,12 @@ from . import views
 app_name = 'commit'
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('detail/', views.detail, name='detail'),
-    path('detail/<int:id>', views.detail_commit, name='detail_commit'),
-    path('detail/comment/<int:id>', views.detail_write_comment,
-         name='detail_writer_comment'),
-    path('detail/comment/delete/<int:id>',
-         views.detail_delete_comment, name='detail_delete-comment'),
-         path('write/', views.write_view, name='write_view'),
-    path('edit/<int:id>/', views.edit_view, name='edit_view'),
+     path('', views.home, name='home'),
+     path('detail/', views.detail, name='detail'),
+     path('detail/<int:id>', views.detail_commit, name='detail_commit'),
+     path('detail/comment/<int:id>', views.detail_write_comment, name='detail_writer_comment'),
+     path('detail/comment/delete/<int:id>', views.detail_delete_comment, name='detail_delete-comment'),
+     path('write/', views.write_view, name='write_view'),
+     path('edit/<int:id>/', views.edit_view, name='edit_view'),
+     path('write/delete/<int:id>', views.delete_view, name='delete_view'),
 ]
--- user/views.py
+++ user/views.py
@@ -8,10 +8,11 @@ from .models import User
 
 def log_in_view(request):
     if request.method == 'POST':
+        username = request.POST.get('username', None)
         email = request.POST.get('email', None)
         password = request.POST.get('password', None)
 
-        me = auth.authenticate(request, email=email, password=password)
+        me = auth.authenticate(request, username=username, password=password)
         if me is not None:
             auth.login(request, me)
             return redirect('commit:home')    # 메인페이지로 가는
