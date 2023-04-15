05ce5ed677420c64f23a2fdf7c0f2793ba1250ee
ellisajung <ellisajung12@gmail.com>
2023-04-15 21:59:10 +0900

    mypage 수정

--- user/models.py
+++ user/models.py
@@ -7,3 +7,5 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     class Meta:
         db_table = 'my_user'
+
+    profile_msg = models.CharField(max_length=30, default='')
